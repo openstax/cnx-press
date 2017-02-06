5445160562c1100defd9dbb0837fb98232368f81
Michael Mulich <michael.mulich@gmail.com>
2017-02-05 18:06:41 -0800

    Initial commit

---
+++ README.rst
@@ -0,0 +1,11 @@
+CNX Content Manager Press
+=========================
+
+This application is an amendment to the Connexions application suite that enables legacy/zope-style publications. This application was created in an effort to decommission the legacy/zope application and connected applications.
+
+License
+-------
+
+This software is subject to the provisions of the GNU Affero General
+Public License Version 3.0 (AGPL). See license.txt for details.
+Copyright (c) 2017 Rice University
---
+++ setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup_requires = (
+    'pytest-runner',
+    )
+install_requires = (
+    )
+tests_require = [
+    'pytest',
+    ]
+extras_require = {
+    'test': tests_require,
+    }
+description = "Connexions Content Manager Press"
+with open('README.rst', 'r') as readme:
+    long_description = readme.read()
+
+setup(
+    name='press',
+    version='0.0.0',
+    author='Connexions team',
+    author_email='info@cnx.org',
+    url="https://github.com/connexions/press",
+    license='AGPL, See also LICENSE.txt',
+    description=description,
+    long_description=long_description,
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require=extras_require,
+    test_suite='press.tests',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'press.tests': ['data/**/*.*'],
+        },
+    entry_points="""\
+    [console_scripts]
+    """,
+    )
