c626865ce9c8ace7c3560b403e356da9b164a095
Michael Mulich <michael.mulich@gmail.com>
2017-11-17 12:13:33 -0800

    Add a swagger api spec for the current api

--- press/views/__init__.py
+++ press/views/__init__.py
@@ -1,4 +1,19 @@
+from pathlib import Path
+
+
+here = Path(__file__).parent
+
+
 def includeme(config):
     """Declaration of routing"""
     add_route = config.add_route
     add_route('ping', '/ping')
+
+    s = config.registry.settings
+    s['pyramid_swagger.exclude_paths'] = [
+        '^/api-docs/?',
+        '^/ping/?',
+    ]
+    s['pyramid_swagger.schema_file'] = 'swagger.yaml'
+    s['pyramid_swagger.schema_directory'] = str(here / 'api-docs')
+    config.include('pyramid_swagger')
