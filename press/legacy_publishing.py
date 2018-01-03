b926e18bd31c18abd83b81b3f1bc3577a477f3f3
Michael Mulich <michael.mulich@gmail.com>
2018-01-03 13:47:24 -0800

    Add stub functions for legacy content publishing

---
+++ press/legacy_publishing.py
@@ -0,0 +1,27 @@
+
+
+
+def publish_litezip(struct, registry):
+    """\
+    ``struct`` is a litezip struct from ``parse_litezip``
+    ``registry`` is a pyramid component architecture registry
+
+    """
+
+
+def publish_legacy_page(model, metadata, registry):
+    """\
+    ``model`` is a ``litezip.Module``
+    ``metadata`` is a ``ModuleMetadata``
+    ``registry`` is a pyramid component architecture registry
+
+    """
+
+
+def publish_legacy_book(model, metadata, registry):
+    """\
+    ``model`` is a ``litezip.Collection``
+    ``metadata`` is a ``CollectionMetadata``
+    ``registry`` is a pyramid component architecture registry
+
+    """
---
+++ tests/unit/test_legacy_publishing.py
@@ -0,0 +1,17 @@
+from press.legacy_publishing import (
+    publish_legacy_book,
+    publish_legacy_page,
+    publish_litezip,
+)
+
+
+# def test_publish_new_legacy_page(db_engines, db_tables):
+#     pass
+
+
+# def test_publish_legacy_book(db_engines, db_tables):
+#     pass
+
+
+# def test_publish_litezip(db_engines, db_tables):
+#     pass
