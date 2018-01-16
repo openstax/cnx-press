86ea113254fadebd5132cc16980b4ff1e9d8ce68
Michael Mulich <michael.mulich@gmail.com>
2018-01-16 12:50:24 -0800

    Add a function to expand a zipfile

--- press/publishing.py
+++ press/publishing.py
@@ -1,10 +1,12 @@
 import tempfile
+import zipfile
 from pathlib import Path
 
 from pyramid.threadlocal import get_current_registry
 
 
 __all__ = (
+    'expand_zip',
     'persist_file_to_filesystem',
 )
 
@@ -21,3 +23,21 @@ def persist_file_to_filesystem(file):
         fb.write(file.read())
     file.seek(0)
     return filepath
+
+
+def expand_zip(filepath):
+    settings = get_current_registry().settings
+    shared_directory = Path(settings['shared_directory'])
+    _names = tempfile._get_candidate_names()
+    while True:
+        dir = shared_directory / next(_names)
+        try:
+            dir.mkdir()
+        except FileExistsError:  # pragma: no cover
+            continue
+        break
+    expand_path = dir
+
+    with filepath.open('rb') as fb, zipfile.ZipFile(fb) as z:
+        z.extractall(path=str(expand_path))
+    return expand_path
