77b8043e94986718c7fb82a084d638c34667b5be
Michael Mulich <michael.mulich@gmail.com>
2018-04-06 14:50:54 -0700

    Add collection tree rebuilding func for republish

--- press/publishing/republish.py
+++ press/publishing/republish.py
@@ -53,13 +53,61 @@ SELECT uuid, major_version, minor_version FROM inserted
 """
 
 
-def republish_collection(trans, uuid, version):
-    """Republish a collection's metadata.
+_collection_tree_sql = text("""\
+WITH RECURSIVE t(nodeid, parent_id, documentid, title, childorder, latest,
+                 uuid, major_version, minor_version, path) AS (
+  SELECT
+    tr.nodeid, tr.parent_id, tr.documentid,
+    tr.title, tr.childorder, tr.latest,
+    m.uuid, m.major_version, m.minor_version,
+    ARRAY[tr.nodeid]
+  FROM trees AS tr
+    JOIN modules AS m ON (tr.documentid = m.module_ident)
+  WHERE tr.documentid = m.module_indent
+    AND m.uuid = :uuid
+    AND m.major_version = :major_version
+    AND m.minor_version = :minor_version
+    AND tr.is_collated = FALSE
+UNION ALL
+  SELECT
+    c.nodeid, c.parent_id, c.documentid, c.title, c.childorder, c.latest,
+    m.uuid, m.major_version, m.minor_version,
+    path || ARRAY[c.nodeid]
+  FROM trees AS c
+    JOIN t ON (c.parent_id = t.nodeid)
+    JOIN modules AS m ON (c.documentid = m.module_ident)
+  WHERE not c.nodeid = ANY(t.path) AND c.is_collated = FALSE
+)
+SELECT row_to_json(row) FROM (SELECT * FROM t) AS row""")
+
+_tree_insert_sql = text("""\
+INSERT INTO trees
+  (nodeid, parent_id,
+   documentid,
+   title, childorder, latest)
+VALUES
+  (DEFAULT, :parent_id,
+   (SELECT module_ident
+    FROM modules
+    WHERE uuid = :uuid
+      AND major_version = :major_version
+      AND minor_version = :minor_version
+    ),
+   :title, :childorder, :latest)
+RETURNING nodeid""")
+
+
+def republish_collection(trans, uuid, version, change_map):
+    """Republish a collection.
 
     :param trans: the database transaction
     :param uuid: uuid of the content
     :param version: version to republish from
     :type version: tuple(int, int)
+    :param change_map: a mapping of uuids to versions for changed
+                       content; the changed content is likely part of the
+                       broader publication
+    :type change_map: dict
     :returns: the published version as a version tuple
               containing major and minor version.
     :rtypes: tuple(int, int)
@@ -76,5 +124,65 @@ def republish_collection(trans, uuid, version):
         next_minor_version=bumped_version[0],
     )
     result = trans.execute(stmt)
-    uuid, major_version, minor_version = result.fetchone()
-    return uuid, (major_version, minor_version)
+    uuid, new_major_version, new_minor_version = result.fetchone()
+
+    # Add this new version to the change map
+    change_map[uuid] = (new_major_version, new_minor_version)
+    # Rebuild the collection tree from the previous version's tree
+    _rebuild_collection_tree(trans, uuid, version, change_map)
+
+    return uuid, (new_major_version, new_minor_version)
+
+
+def _rebuild_collection_tree(trans, uuid, version, change_map):
+    """
+
+    Rebuild the collection tree based on the previous version's tree
+    but with references to the newly published modules.
+
+    :param trans: the database transaction
+    :param uuid: uuid of the content
+    :param version: version to republish from
+    :type version: tuple(int, int)
+    :param change_map: a mapping of uuids to versions for changed
+                       content; the changed content is likely part of the
+                       broader publication
+    :type change_map: dict
+
+    """
+    (major_version, minor_version) = version
+
+    def get_tree():
+        result = trans.execute(
+            _collection_tree_sql,
+            uuid=uuid,
+            major_version=major_version,
+            minor_version=minor_version,
+        )
+        for row in result:
+            yield row[0]
+
+    def insert(fields):
+        result = trans.execute(_tree_insert_sql, **fields)
+        return result.fetchone()[0]
+
+    tree = {}  # {<current-nodeid>: {<row-data>...}, ...}
+    children = {}  # {<nodeid>: [<child-nodeid>, ...], <child-nodeid>: [...]}
+    for node in get_tree():
+        tree[node['nodeid']] = node
+        children.setdefault(node['parent_id'], [])
+        children[node['parent_id']].append(node['nodeid'])
+
+    def build_tree(nodeid, parent_id=None):
+        node = tree[nodeid]
+        node['parent_id'] = parent_id
+        if change_map.get(node['uuid']) is not None \
+           and (node['latest'] or parent_id is None):
+            node['major_version'] = change_map[node['uuid']][0]
+            node['minor_version'] = change_map[node['uuid']][1]
+        new_nodeid = insert(node)
+        for child_nodeid in children.get(nodeid, []):
+            build_tree(child_nodeid, new_nodeid)
+
+    root_node = children[None][0]
+    build_tree(root_node)
--- tests/unit/publishing/test_republish.py
+++ tests/unit/publishing/test_republish.py
@@ -1,6 +1,22 @@
 import pretend
 
-from press.publishing.republish import republish_collection
+from press.publishing.republish import (
+    _rebuild_collection_tree,
+    republish_collection
+)
+
+
+# To enable an ANY value when matching with pretend.call_recorder
+# From https://github.com/alex/pretend/issues/7
+class _Any:
+    def __eq__(self, other):
+        return True
+
+    def __ne__(self, other):
+        return False
+
+
+ANY = _Any()
 
 
 class TestRepublishCollection:
@@ -9,12 +25,19 @@ class TestRepublishCollection:
         uuid = '<uuid>'
         major_version, minor_version = (4, 5)
         next_mjr_version, next_mnr_version = major_version, minor_version + 1
+        change_map = {}
+
         bump_version = pretend.call_recorder(
             lambda t, i, is_minor_bump: (major_version, minor_version + 1))
-
         monkeypatch.setattr(
             'press.publishing.republish.bump_version',
             bump_version
+        )
+        _rebuild_collection_tree = pretend.call_recorder(
+            lambda trans, uuid, version, change_map: None)
+        monkeypatch.setattr(
+            'press.publishing.republish._rebuild_collection_tree',
+            _rebuild_collection_tree
         )
 
         row = (uuid, next_mjr_version, next_mnr_version)
@@ -22,10 +45,99 @@ class TestRepublishCollection:
         db_transaction = pretend.stub(execute=lambda _: db_result)
 
         id, version = republish_collection(
-            db_transaction, uuid, (major_version, minor_version))
+            db_transaction,
+            uuid,
+            (major_version, minor_version),
+            change_map,
+        )
 
         assert id == uuid
         assert version == (next_mjr_version, next_mnr_version)
 
+        assert change_map == {uuid: (next_mjr_version, next_mnr_version)}
+
         expected_call = pretend.call(db_transaction, uuid, is_minor_bump=True)
         assert bump_version.calls == [expected_call]
+
+
+class TestRebuildCollectionTree:
+
+    def test(self):
+        uuid = '<uuid>'
+        major_version, minor_version = (4, 5)
+        next_mjr_version, next_mnr_version = major_version, minor_version + 1
+        change_map = {
+            uuid: (next_mjr_version, next_mnr_version),
+            '<uuid-22>': (2, None),
+            '<uuid-44>': (2, None),
+        }
+
+        tree_row_keys = [
+            'nodeid', 'parent_id', 'documentid', 'title',
+            'childorder', 'latest',
+            'uuid', 'major_version', 'minor_version', 'path',
+        ]
+        tree_rows = [
+            (1, None, 11, None, 0, None,
+             uuid, major_version, minor_version, [1]),
+            (2, 1, 22, 'twenty-two', 1, True,  # update to latest
+             '<uuid-22>', 1, None, [1, 2]),
+            (3, 1, 33, 'subcol', 2, None,
+             '<uuid-33>', 2, 1, [1, 3]),
+            (4, 3, 44, None, 3, False,  # do not update to latest
+             '<uuid-44>', 1, None, [1, 3, 4]),
+            (5, 3, 55, 'fifty-five', 4, None,
+             '<uuid-55>', 1, None, [1, 3, 5])
+        ]
+        tree_rows = list([(dict(zip(tree_row_keys, x)),) for x in tree_rows])
+
+        db_results = [
+            # call for the tree data, which is iterated over
+            pretend.stub(__iter__=lambda: iter(tree_rows)),
+            # individual calls for a tree node insertion, returning nodeid
+            pretend.stub(fetchone=lambda: (6,)),
+            pretend.stub(fetchone=lambda: (7,)),
+            pretend.stub(fetchone=lambda: (8,)),
+            pretend.stub(fetchone=lambda: (9,)),
+            pretend.stub(fetchone=lambda: (10,)),
+        ]
+        execute = pretend.call_recorder(lambda *a, **kw: db_results.pop(0))
+        db_transaction = pretend.stub(execute=execute)
+
+        _rebuild_collection_tree(
+            db_transaction,
+            uuid,
+            (major_version, minor_version),
+            change_map,
+        )
+
+        # Check the insert was called with the correct parameters
+        # Note, the nodeid and path are ignored by the insert statement.
+        expected_insert_calls = [
+            # Note the change in the minor version
+            pretend.call(ANY, nodeid=ANY, parent_id=None, documentid=11,
+                         title=None, childorder=0, latest=None,
+                         uuid='<uuid>', major_version=4, minor_version=6,
+                         path=ANY),
+            # Note the change in the parent and version
+            pretend.call(ANY, nodeid=ANY, parent_id=6, documentid=22,
+                         title='twenty-two', childorder=1, latest=True,
+                         uuid='<uuid-22>', major_version=2,
+                         minor_version=None, path=ANY),
+            # Note the change in the parent
+            pretend.call(ANY, nodeid=ANY, parent_id=6, documentid=33,
+                         title='subcol', childorder=2, latest=None,
+                         uuid='<uuid-33>', major_version=2, minor_version=1,
+                         path=ANY),
+            # Note the change in the parent and specifically not the version
+            pretend.call(ANY, nodeid=ANY, parent_id=8, documentid=44,
+                         title=None, childorder=3, latest=False,
+                         uuid='<uuid-44>', major_version=1,
+                         minor_version=None, path=ANY),
+            # Note the change in the parent
+            pretend.call(ANY, nodeid=ANY, parent_id=8, documentid=55,
+                         title='fifty-five', childorder=4, latest=None,
+                         uuid='<uuid-55>', major_version=1,
+                         minor_version=None, path=ANY)]
+        # Ignore the first execute call which retrieves the tree
+        assert execute.calls[1:] == expected_insert_calls
