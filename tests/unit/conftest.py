297acb60964c36441bd52e89bbc729d1547f64cc
Michael Mulich <michael.mulich@gmail.com>
2018-04-07 10:06:52 -0700

    Move app fixture into the common usage conftest module

--- tests/unit/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from pyramid import testing as pyramid_testing
-
-
-@pytest.fixture
-def app(env_vars):
-    from press.config import configure
-    yield configure()
-    pyramid_testing.tearDown()
