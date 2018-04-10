a5d052627e435be56da7677dbfa2da52fed54610
Michael Mulich <michael.mulich@gmail.com>
2018-04-09 20:49:15 -0700

    Copy the modules list so we don't append other modules to it

--- tests/conftest.py
+++ tests/conftest.py
@@ -357,6 +357,7 @@ class _ContentUtil:
 
         """
         tree = []
+        modules = modules.copy()
         for module in modules:
             node = self.make_tree_node_from(module)
             tree.append(node)
