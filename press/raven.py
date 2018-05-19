867bbe96a063be324d4be5224281a1e27071f457
Michael Mulich <michael.mulich@gmail.com>
2018-05-18 21:59:08 -0700

    Add the raven client as a request method

--- press/raven.py
+++ press/raven.py
@@ -2,8 +2,16 @@ from pyramid import tweens
 import raven
 
 
+def _client_factory(registry):
+    return raven.Client(registry.settings.get('sentry.dsn'))
+
+
 def raven_tween_factory(handler, registry):
-    client = raven.Client(registry.settings.get('sentry.dsn'))
+    """A tween factory that registers a tween
+    that will capture uncaught exceptions.
+
+    """
+    client = _client_factory(registry)
 
     def tween(request):
         try:
@@ -18,10 +26,23 @@ def raven_tween_factory(handler, registry):
     return tween
 
 
+def _create_raven_client(request):
+    """Request attribute factory to give access non-critical exception
+    handling code a way to report issues.
+
+    """
+    return _client_factory(request.registry)
+
+
 def includeme(config):
     """Integrates Sentry's Raven client"""
     config.add_tween(
         'press.raven.raven_tween_factory',
         under=tweens.INGRESS,
         over=tweens.EXCVIEW,
+    )
+    config.add_request_method(
+        _create_raven_client,
+        name='raven_client',
+        reify=True,
     )
--- tests/unit/test_raven.py
+++ tests/unit/test_raven.py
@@ -2,7 +2,7 @@ import pretend
 import pytest
 from pyramid import tweens
 
-from press import raven
+from press import raven as press_raven
 
 
 class FauxException(Exception):
@@ -18,7 +18,7 @@ def test_tween_without_error(monkeypatch):
         context=pretend.stub(clear=context_clear),
     )
     client_factory = pretend.call_recorder(lambda dsn: client)
-    monkeypatch.setattr(raven.raven, 'Client', client_factory)
+    monkeypatch.setattr(press_raven.raven, 'Client', client_factory)
 
     # Stub out the calls to the registry
     test_dsn = '<dsn>'
@@ -28,7 +28,7 @@ def test_tween_without_error(monkeypatch):
     def handler(request):
         pass
 
-    tween = raven.raven_tween_factory(handler, registry)
+    tween = press_raven.raven_tween_factory(handler, registry)
 
     request = object()  # marker object
     tween(request)
@@ -48,7 +48,7 @@ def test_tween_with_error(monkeypatch):
         context=pretend.stub(clear=context_clear),
     )
     client_factory = pretend.call_recorder(lambda dsn: client)
-    monkeypatch.setattr(raven.raven, 'Client', client_factory)
+    monkeypatch.setattr(press_raven.raven, 'Client', client_factory)
 
     # Stub out the calls to the registry
     test_dsn = '<dsn>'
@@ -58,7 +58,7 @@ def test_tween_with_error(monkeypatch):
     def handler(request):
         raise FauxException
 
-    tween = raven.raven_tween_factory(handler, registry)
+    tween = press_raven.raven_tween_factory(handler, registry)
 
     request = object()  # marker object
     with pytest.raises(FauxException):
@@ -73,15 +73,29 @@ def test_tween_with_error(monkeypatch):
 def test_includeme():
     # FIXME Need to do under and over
     add_tween = pretend.call_recorder(lambda factory, over, under: None)
-    config = pretend.stub(add_tween=add_tween)
+    add_request_method = pretend.call_recorder(lambda fn, name, reify: None)
+    config = pretend.stub(
+        add_tween=add_tween,
+        add_request_method=add_request_method,
+    )
 
     # Call the target includeme
-    raven.includeme(config)
+    press_raven.includeme(config)
 
+    # Ensure the tween was registered
     assert config.add_tween.calls == [
         pretend.call(
             'press.raven.raven_tween_factory',
             under=tweens.INGRESS,
             over=tweens.EXCVIEW,
+        ),
+    ]
+
+    # Ensure the raven_client request attribute was registered
+    assert config.add_request_method.calls == [
+        pretend.call(
+            press_raven._create_raven_client,
+            name='raven_client',
+            reify=True,
         ),
     ]
