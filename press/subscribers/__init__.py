3d14d49b83d0903a04ea64b21e2acdc70c1071cd
Ross Reedstrom <reedstrm@gmail.com>
2018-06-01 10:11:13 -0500

    add another subscriber to trigger update of legacy latest (for collections especially)

--- press/subscribers/__init__.py
+++ press/subscribers/__init__.py
@@ -3,6 +3,7 @@ from pyramid import events as pyramid_events
 from press import events
 
 from .legacy_enqueue import legacy_enqueue as _legacy_enqueue
+from .legacy_update_latest import legacy_update_latest as _legacy_update_latest
 from .purge_cache import purge_cache as _purge_cache
 from .track_pubs import (
     create_tracked_pubs_location,
@@ -11,6 +12,10 @@ from .track_pubs import (
 
 
 def includeme(config):
+    config.add_subscriber(
+        _legacy_update_latest,
+        events.LegacyPublicationFinished,
+    )
     config.add_subscriber(
         _legacy_enqueue,
         events.LegacyPublicationFinished,
