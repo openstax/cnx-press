a8d31d60a5188ccf4c1633d3f2b0f5e5445d4041
Michael Mulich <michael.mulich@gmail.com>
2018-06-07 09:36:15 -0700

    Fix test coverage for retrying requests to legacy enqueue

--- tests/unit/subscribers/test_legacy_enqueue.py
+++ tests/unit/subscribers/test_legacy_enqueue.py
@@ -3,8 +3,11 @@ import requests
 import requests_mock as rmock
 
 from press.events import LegacyPublicationFinished
-
 from press.subscribers.legacy_enqueue import legacy_enqueue
+
+from tests.helpers import (
+    retryable_timeout_request_mock_callback
+)
 
 
 def test(requests_mock):
@@ -45,7 +48,7 @@ def test(requests_mock):
         assert id in logger_info.calls[i].args[0]
 
 
-def test_failed_request(requests_mock):
+def test_failed_request_and_retry_failed(requests_mock):
     ids = [
         ('m12345', (2, None)),
         ('m54321', (4, None)),
@@ -79,8 +82,6 @@ def test_failed_request(requests_mock):
     # Call the subcriber
     legacy_enqueue(event)
 
-    # TODO have the other requests succeeded?
-
     # Check raven was used...
     assert captureException.calls
 
@@ -91,3 +92,45 @@ def test_failed_request(requests_mock):
     assert len(logger_info.calls) == len(ids) - 1
     for i, (id, ver) in enumerate(sorted(ids)[:-1]):
         assert id in logger_info.calls[i].args[0]
+
+
+def test_failed_request_and_retry_success(requests_mock):
+    ids = [
+        ('m12345', (2, None)),
+        ('m54321', (4, None)),
+        ('col32154', (5, 1)),
+    ]
+
+    @retryable_timeout_request_mock_callback
+    def request_callback(request, context, tries):
+        # fails at first but succeeds on the second try
+        if ids[1][0] in request.url and tries < 2:
+            raise requests.exceptions.ConnectTimeout
+        return 'enqueued'
+
+    # mock a problem request to enqueue
+    requests_mock.register_uri('GET', rmock.ANY, text=request_callback)
+
+    # stub out the raven client
+    captureException = pretend.call_recorder(lambda: None)
+    raven_client = pretend.stub(captureException=captureException)
+    # stub out the logger
+    logger_info = pretend.call_recorder(lambda *a, **kw: None)
+    logger_exception = pretend.call_recorder(lambda *a, **kw: None)
+    logger = pretend.stub(
+        info=logger_info,
+        exception=logger_exception,
+    )
+    # Create an event with a stub request
+    request = pretend.stub(domain='example.org', scheme='mock',
+                           log=logger, raven_client=raven_client)
+    event = LegacyPublicationFinished(ids, request)
+
+    # Call the subcriber
+    legacy_enqueue(event)
+
+    # Check raven was used...
+    assert captureException.calls
+
+    # Check for logging
+    assert logger_exception.calls == []
