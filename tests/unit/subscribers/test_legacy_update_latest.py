ce00428aaa30574c66172232c4ded76ab87d1bfc
Michael Mulich <michael.mulich@gmail.com>
2018-07-27 10:37:28 -0700

    Refactor subscriber tests to use a common request fixture

--- tests/unit/subscribers/test_legacy_update_latest.py
+++ tests/unit/subscribers/test_legacy_update_latest.py
@@ -10,23 +10,19 @@ from tests.helpers import (
 )
 
 
-def test(requests_mock):
+def test(requests_mock, event_request):
     request_callback = pretend.call_recorder(
         lambda request, context: 'updated')
     # mock the request to enqueue
     requests_mock.register_uri('GET', rmock.ANY, text=request_callback)
 
-    # stub out the logger
-    logger_info = pretend.call_recorder(lambda *a, **kw: None)
-    logger = pretend.stub(info=logger_info)
     # Create an event with a stub request
     ids = [
         ('m12345', (2, None)),
         ('m54321', (4, None)),
         ('col32154', (5, 1)),
     ]
-    request = pretend.stub(domain='example.org', scheme='mock', log=logger)
-    event = LegacyPublicationFinished(ids, request)
+    event = LegacyPublicationFinished(ids, event_request)
 
     # Call the subcriber
     legacy_update_latest(event)
@@ -42,12 +38,12 @@ def test(requests_mock):
         assert url == request.url
 
     # Check for logging
-    assert len(logger_info.calls) == len(ids)
+    assert len(event_request.log.info.calls) == len(ids)
     for i, (id, ver) in enumerate(sorted(ids)):
-        assert id in logger_info.calls[i].args[0]
+        assert id in event_request.log.info.calls[i].args[0]
 
 
-def test_failed_request_and_retry_failed(requests_mock):
+def test_failed_request_and_retry_failed(requests_mock, event_request):
     ids = [
         ('m12345', (2, None)),
         ('m54321', (4, None)),
@@ -63,37 +59,24 @@ def test_failed_request_and_retry_failed(requests_mock):
     # mock a problem request to enqueue
     requests_mock.register_uri('GET', rmock.ANY, text=request_callback)
 
-    # stub out the raven client
-    captureException = pretend.call_recorder(lambda: None)
-    raven_client = pretend.stub(captureException=captureException)
-    # stub out the logger
-    logger_info = pretend.call_recorder(lambda *a, **kw: None)
-    logger_exception = pretend.call_recorder(lambda *a, **kw: None)
-    logger = pretend.stub(
-        info=logger_info,
-        exception=logger_exception,
-    )
-    # Create an event with a stub request
-    request = pretend.stub(domain='example.org', scheme='mock',
-                           log=logger, raven_client=raven_client)
-    event = LegacyPublicationFinished(ids, request)
+    event = LegacyPublicationFinished(ids, event_request)
 
     # Call the subcriber
     legacy_update_latest(event)
 
     # Check raven was used...
-    assert captureException.calls
+    assert event_request.raven_client.captureException.calls
 
     # Check for logging
-    assert logger_exception.calls == [
+    assert event_request.log.exception.calls == [
         pretend.call("problem fetching '{}'".format(ids[1][0])),
     ]
-    assert len(logger_info.calls) == len(ids) - 1
+    assert len(event_request.log.info.calls) == len(ids) - 1
     for i, (id, ver) in enumerate(sorted(ids)[:-1]):
-        assert id in logger_info.calls[i].args[0]
+        assert id in event_request.log.info.calls[i].args[0]
 
 
-def test_failed_request_and_retry_success(requests_mock):
+def test_failed_request_and_retry_success(requests_mock, event_request):
     ids = [
         ('m12345', (2, None)),
         ('m54321', (4, None)),
@@ -110,26 +93,13 @@ def test_failed_request_and_retry_success(requests_mock):
     # mock a problem request to enqueue
     requests_mock.register_uri('GET', rmock.ANY, text=request_callback)
 
-    # stub out the raven client
-    captureException = pretend.call_recorder(lambda: None)
-    raven_client = pretend.stub(captureException=captureException)
-    # stub out the logger
-    logger_info = pretend.call_recorder(lambda *a, **kw: None)
-    logger_exception = pretend.call_recorder(lambda *a, **kw: None)
-    logger = pretend.stub(
-        info=logger_info,
-        exception=logger_exception,
-    )
-    # Create an event with a stub request
-    request = pretend.stub(domain='example.org', scheme='mock',
-                           log=logger, raven_client=raven_client)
-    event = LegacyPublicationFinished(ids, request)
+    event = LegacyPublicationFinished(ids, event_request)
 
     # Call the subcriber
     legacy_update_latest(event)
 
     # Check raven was used...
-    assert captureException.calls
+    assert event_request.raven_client.captureException.calls
 
     # Check for logging
-    assert logger_exception.calls == []
+    assert event_request.log.exception.calls == []
