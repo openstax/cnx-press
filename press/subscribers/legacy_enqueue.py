047bf541a2b409fbe757e6df0071cd7ade3daa78
Michael Mulich <michael.mulich@gmail.com>
2018-07-30 23:37:44 -0700

    Adjust legacy_enqueue's logic to use an async celery task

--- press/subscribers/legacy_enqueue.py
+++ press/subscribers/legacy_enqueue.py
@@ -1,44 +1,36 @@
-import requests
-
+from press.outofband import make_request
 from press.utils import convert_to_legacy_domain
+
+
+# The url is built specifically for collections, but works for modules,
+# because the code for modules doesn't care about the extra options.
+URL_TMPLT = (
+    '{base_url}/content/{module_id}/{version}'
+    '/enqueue?colcomplete=True&collxml=True'
+)
 
 
 # subscriber for press.events.LegacyPublicationFinished
 def legacy_enqueue(event):
     logger = event.request.log
 
+    # Get the celery task object
+    task_path = '.'.join([make_request.__module__, make_request.__name__])
+    _make_request = event.request.registry.celery_app.tasks[task_path]
+
     # Build the enqueue RPC url
     domain = convert_to_legacy_domain(event.request.domain)
     scheme = event.request.scheme
     base_url = '{}://{}'.format(scheme, domain)
-    # The url is built specifically for collections, but works for modules,
-    # because the code for modules doesn't care about the extra options.
-    url_tmplt = (
-        '{base_url}/content/{module_id}/{version}'
-        '/enqueue?colcomplete=True&collxml=True'
-    )
 
-    timeout = (1, 20)  # (<connect>, <read>)
-    ids = sorted(event.ids)
-    with requests.Session() as session:
-        for id, ver in ids:
-            version = '1.{}'.format(ver[0])
-            url = url_tmplt.format(
-                base_url=base_url,
-                module_id=id,
-                version=version,
-            )
-            try:
-                session.get(url, timeout=timeout)
-            except requests.exceptions.RequestException:
-                event.request.raven_client.captureException()
-                # try one more time
-                try:
-                    session.get(url, timeout=timeout)
-                except requests.exceptions.RequestException:
-                    event.request.raven_client.captureException()
-                    logger.exception("problem enqueuing '{}'".format(id))
-                    continue
-                continue
-            logger.info("enqueued '{}' within the legacy system"
-                        .format(id))
+    for id, ver in sorted(event.ids):
+        version = '1.{}'.format(ver[0])
+        url = URL_TMPLT.format(
+            base_url=base_url,
+            module_id=id,
+            version=version,
+        )
+        _make_request.delay(url)
+        logger.info(
+            "asynchronously enqueued '{}' within the legacy system".format(id)
+        )
--- tests/unit/subscribers/test_legacy_enqueue.py
+++ tests/unit/subscribers/test_legacy_enqueue.py
@@ -1,20 +1,20 @@
 import pretend
-import requests
-import requests_mock as rmock
 
 from press.events import LegacyPublicationFinished
+from press.outofband import make_request
 from press.subscribers.legacy_enqueue import legacy_enqueue
 
-from tests.helpers import (
-    retryable_timeout_request_mock_callback
-)
+
+def _make_url(info):
+    id, ver = info
+    known_base_url = 'mock://legacy.example.org'
+    return (
+        '{}/content/{}/{}/enqueue?colcomplete=True&collxml=True'
+        .format(known_base_url, id, '1.{}'.format(ver[0]))
+    )
 
 
 def test(requests_mock, stub_request):
-    request_callback = pretend.call_recorder(
-        lambda request, context: 'enqueued')
-    # mock the request to enqueue
-    requests_mock.register_uri('GET', rmock.ANY, text=request_callback)
 
     # Create an event with a stub request
     ids = [
@@ -27,80 +27,13 @@ def test(requests_mock, stub_request):
     # Call the subcriber
     legacy_enqueue(event)
 
-    # Check for request calls
-    assert len(request_callback.calls) == len(ids)
-    known_base_url = 'mock://legacy.example.org'
-    for i, (id, ver) in enumerate(sorted(ids)):
-        url = (
-            '{}/content/{}/{}/enqueue?colcomplete=True&collxml=True'
-            .format(known_base_url, id, '1.{}'.format(ver[0]))
-        )
-        request, context = request_callback.calls[i].args
-        assert url == request.url
+    # Check for task calls
+    task_path = '.'.join([make_request.__module__, make_request.__name__])
+    task = stub_request.registry.celery_app.tasks[task_path]
+    expected_calls = list(map(pretend.call, map(_make_url, sorted(ids))))
+    assert task.delay.calls == expected_calls
 
     # Check for logging
     assert len(stub_request.log.info.calls) == len(ids)
     for i, (id, ver) in enumerate(sorted(ids)):
         assert id in stub_request.log.info.calls[i].args[0]
-
-
-def test_failed_request_and_retry_failed(requests_mock, stub_request):
-    ids = [
-        ('m12345', (2, None)),
-        ('m54321', (4, None)),
-        ('col32154', (5, 1)),
-    ]
-
-    def request_callback(request, context):
-        if ids[1][0] in request.url:
-            raise requests.exceptions.ConnectTimeout
-        else:
-            return 'enqueued'
-
-    # mock a problem request to enqueue
-    requests_mock.register_uri('GET', rmock.ANY, text=request_callback)
-
-    event = LegacyPublicationFinished(ids, stub_request)
-
-    # Call the subcriber
-    legacy_enqueue(event)
-
-    # Check raven was used...
-    assert stub_request.raven_client.captureException.calls
-
-    # Check for logging
-    assert stub_request.log.exception.calls == [
-        pretend.call("problem enqueuing '{}'".format(ids[1][0])),
-    ]
-    assert len(stub_request.log.info.calls) == len(ids) - 1
-    for i, (id, ver) in enumerate(sorted(ids)[:-1]):
-        assert id in stub_request.log.info.calls[i].args[0]
-
-
-def test_failed_request_and_retry_success(requests_mock, stub_request):
-    ids = [
-        ('m12345', (2, None)),
-        ('m54321', (4, None)),
-        ('col32154', (5, 1)),
-    ]
-
-    @retryable_timeout_request_mock_callback
-    def request_callback(request, context, tries):
-        # fails at first but succeeds on the second try
-        if ids[1][0] in request.url and tries < 2:
-            raise requests.exceptions.ConnectTimeout
-        return 'enqueued'
-
-    # mock a problem request to enqueue
-    requests_mock.register_uri('GET', rmock.ANY, text=request_callback)
-
-    event = LegacyPublicationFinished(ids, stub_request)
-
-    # Call the subcriber
-    legacy_enqueue(event)
-
-    # Check raven was used...
-    assert stub_request.raven_client.captureException.calls
-
-    # Check for logging
-    assert stub_request.log.exception.calls == []
