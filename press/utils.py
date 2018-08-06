47254acd7d03183c51f074ffe41ff647587df5a7
Michael Mulich <michael.mulich@gmail.com>
2018-08-06 01:01:26 -0700

    Add utility function to produce sha1 and md5 for files

--- press/utils.py
+++ press/utils.py
@@ -1,8 +1,15 @@
+import hashlib
+
+
 __all__ = (
     'convert_to_legacy_domain',
     'convert_version_tuple_to_version_string',
     'convert_version_to_legacy_version',
+    'produce_hashes_from_filepath',
 )
+
+
+BUFFER_CHUNK_SIZE = 131072  # 128kb chunks
 
 
 def convert_version_tuple_to_version_string(version):
@@ -29,3 +36,25 @@ def convert_to_legacy_domain(domain):
     """Given the existing domain, convert it to the legacy domain."""
     sep = len(domain.split('.')) > 2 and '-' or '.'
     return 'legacy{}{}'.format(sep, domain)
+
+
+def produce_hashes_from_filepath(filepath):
+    """Given a filepath to a file, produce the SHA1 and MD5 for that file.
+
+    :param filepath: a filesystem path to the file
+    :type filepath: :class:`pathlib.Path`
+
+    """
+    sha1 = hashlib.sha1()
+    md5 = hashlib.md5()
+    with filepath.open('rb') as fb:
+        while True:
+            data = fb.read(BUFFER_CHUNK_SIZE)
+            if not data:
+                break
+            sha1.update(data)
+            md5.update(data)
+    return {
+        'sha1': sha1.hexdigest(),
+        'md5': md5.hexdigest(),
+    }
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,6 +1,16 @@
+import hashlib
+from pathlib import Path
+
 from press.utils import (
     convert_to_legacy_domain,
     convert_version_to_legacy_version,
+    produce_hashes_from_filepath,
+)
+
+from tests.random_image import (
+    generate_image_by_output_size,
+    generate_random_image_by_size,
+    save_image,
 )
 
 
@@ -27,3 +37,46 @@ class TestConvertToLegacyDomain:
         domain = 'dev.example.com'
         expected = '-'.join(['legacy', domain])
         assert convert_to_legacy_domain(domain) == expected
+
+
+class TestProduceHashesFromFilepath:
+
+    def get_sha1(self, filepath):
+        sha1 = hashlib.sha1()
+        with filepath.open('rb') as fb:
+            sha1.update(fb.read())
+        return sha1.hexdigest()
+
+    def get_md5(self, filepath):
+        md5 = hashlib.md5()
+        with filepath.open('rb') as fb:
+            md5.update(fb.read())
+        return md5.hexdigest()
+
+    def test(self, tmpdir):
+        filepath = Path(tmpdir.mkdir('img')) / 'file.jpeg'
+
+        img = generate_random_image_by_size(10)
+        save_image(img, str(filepath))
+
+        hashes = produce_hashes_from_filepath(filepath)
+
+        expected = {
+            'sha1': self.get_sha1(filepath),
+            'md5': self.get_md5(filepath),
+        }
+        assert hashes == expected
+
+    def test_large_file(self, tmpdir):
+        filepath = Path(tmpdir.mkdir('img')) / 'big_file.jpeg'
+
+        img = generate_image_by_output_size(10240)
+        save_image(img, str(filepath))
+
+        hashes = produce_hashes_from_filepath(filepath)
+
+        expected = {
+            'sha1': self.get_sha1(filepath),
+            'md5': self.get_md5(filepath),
+        }
+        assert hashes == expected
