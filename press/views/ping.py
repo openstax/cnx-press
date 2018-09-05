3498d19cec47deb948f37616576275d62eb80c50
Dante Soares <dante.m.soares@rice.edu>
2018-09-05 13:45:12 -0500

    Added some cache headers for CNX Press

--- press/views/ping.py
+++ press/views/ping.py
@@ -3,7 +3,7 @@ from pyramid.response import Response
 from pyramid.view import view_config
 
 
-@view_config(route_name='ping')
+@view_config(route_name='ping', http_cache=0)
 def ping(request):
     """A ping and ack view for checking the service is up and running."""
     return Response('pong')
