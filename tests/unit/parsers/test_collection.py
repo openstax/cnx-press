06db5a36f8f548dbabcd55bac4b8b853aa085980
BryanEliDimas <bryanelidimas@gmail.com>
2018-09-10 18:47:58 -0500

    Improve test expectation for parsing collxml files

--- tests/unit/parsers/test_collection.py
+++ tests/unit/parsers/test_collection.py
@@ -13,34 +13,25 @@ def test_parse_collxml(collxml_templates):
         expected = parse_collxml(origin)
 
     assert isinstance(expected, CollectionElement)
-    assert expected.name == 'collxml'
+    assert expected.tag == 'collxml'
 
 
 def test_markup_in_title_gets_parsed(collxml_templates):
     """Modules with markup in title gets parsed as part of the title.
     """
     titles_with_markup = [
-        'Introduction to Quartus and Circuit Diagram Design SOME MATH',
-        'Lab 1-1: 4-Bit Mux and all NAND/NOR Mux SOME STYLED TEXT',
-        'Lab 4-1 Interrupt Driven Programming in MSP430 Assembly '
-        'SOME DIV TAG SOME SPAN TAG',
+        'Introduction to SOME MATH Quartus and Circuit Diagram Design',
+        'Lab 1-1: 4-Bit Mux and SOME STYLED TEXT all NAND/NOR Mux',
+        'Lab 4-1 Interrupt Driven SOME SOME SPAN TAG DIV TAG '
+        'Programming in MSP430 Assembly',
     ]
-
-    # TODO: ideally, the text that gets parsed should be the following
-    # (but it's tricky to do with a SAX parser):
-    # titles_with_markup = [
-    #     'Introduction to SOME MATH Quartus and Circuit Diagram Design',
-    #     'Lab 1-1: 4-Bit Mux and SOME STYLED TEXT all NAND/NOR Mux',
-    #     'Lab 4-1 Interrupt Driven SOME DIV TAG SOME SPAN TAG '
-    #     'Programming in MSP430 Assembly',
-    # ]
 
     with (collxml_templates / 'markup_in_title.xml').open('r') as doc:
         tree = parse_collxml(doc)
 
     for title in titles_with_markup:
         assert title in [node._complete_title() for node in tree.traverse()
-                         if node.name == 'title']
+                         if node.tag == 'title']
 
 
 def test_parse_collection_metadata(litezip_valid_litezip):
