5cf44f7151311d344c09a28793c46292cf2c5782
BryanEliDimas <bryanelidimas@gmail.com>
2018-09-13 22:30:45 -0500

    Test authenticated ping

--- tests/functional/views/test_ping.py
+++ tests/functional/views/test_ping.py
@@ -3,3 +3,15 @@ def test_successful_ping(webapp):
     resp = webapp.get('/ping')
     assert resp.status_code == 200
     assert b'pong' in resp.body
+
+
+def test_authenticated_ping_denied(webapp):
+    resp = webapp.get('/auth-ping')
+    assert resp.status_code == 401
+
+
+def test_authenticated_ping_success(webapp):
+    webapp.authorization = ('Basic', ('user1', 'password'))
+    resp = webapp.get('/auth-ping')
+    assert resp.status_code == 200
+    assert b'pong' in resp.body
