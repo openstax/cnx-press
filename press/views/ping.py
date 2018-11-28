dd6993a95a1a99253c4d51b222eee07b7c9e38dd
Ross Reedstrom <reedstrm@gmail.com>
2018-11-28 15:40:05 -0600

    use string render - test content_type

--- press/views/ping.py
+++ press/views/ping.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-from pyramid.response import Response
 from pyramid.view import view_config
 
 
-@view_config(renderer='text', route_name='ping', http_cache=0)
-@view_config(route_name='api-ping', http_cache=0)
-@view_config(route_name='auth-ping', http_cache=0, permission='view')
-@view_config(route_name='publish-ping', http_cache=0, permission='publish')
+@view_config(renderer='string', route_name='ping', http_cache=0)
+@view_config(renderer='string', route_name='api-ping', http_cache=0)
+@view_config(renderer='string', route_name='auth-ping', http_cache=0,
+             permission='view')
+@view_config(renderer='string', route_name='publish-ping', http_cache=0,
+             permission='publish')
 def ping(request):
     """A ping and ack view for checking the service is up and running.
 
     There are multiple views to accommodate multiple paths
     and different permissions"""
-    return Response('pong {path}'.format(path=request.path),
-                    content_type='text/plain')
+    return 'pong {path}'.format(path=request.path)
--- tests/functional/views/test_ping.py
+++ tests/functional/views/test_ping.py
@@ -1,24 +1,35 @@
 
 def test_successful_ping(webapp):
+    resp = webapp.get('/ping')
+    assert resp.status_code == 200
+    assert resp.content_type == 'text/plain'
+    assert b'pong' in resp.body
+
+
+def test_successful_api_ping(webapp):
     resp = webapp.get('/api/ping')
     assert resp.status_code == 200
+    assert resp.content_type == 'text/plain'
     assert b'pong' in resp.body
 
 
 def test_authenticated_ping_no_auth_denied(webapp):
     resp = webapp.get('/api/auth-ping', expect_errors=True)
+    assert resp.content_type == 'application/json'
     assert resp.status_code == 401
 
 
 def test_authenticated_ping_bad_pass_auth_denied(webapp):
     resp = webapp.get('/api/auth-ping', expect_errors=True)
     webapp.authorization = ('Basic', ('user1', 'notapassword'))
+    assert resp.content_type == 'application/json'
     assert resp.status_code == 401
 
 
 def test_authenticated_ping_bad_user_auth_denied(webapp):
     resp = webapp.get('/api/auth-ping', expect_errors=True)
     webapp.authorization = ('Basic', ('notauser', 'foobar'))
+    assert resp.content_type == 'application/json'
     assert resp.status_code == 401
 
 
@@ -26,6 +37,7 @@ def test_authenticated_ping_success(webapp):
     webapp.authorization = ('Basic', ('user1', 'foobar'))
     resp = webapp.get('/api/auth-ping')
     assert resp.status_code == 200
+    assert resp.content_type == 'text/plain'
     assert b'pong' in resp.body
 
 
@@ -33,6 +45,7 @@ def test_publish_ping_success(webapp):
     webapp.authorization = ('Basic', ('user1', 'foobar'))
     resp = webapp.get('/api/publish-ping')
     assert resp.status_code == 200
+    assert resp.content_type == 'text/plain'
     assert b'pong' in resp.body
 
 
@@ -40,3 +53,4 @@ def test_publish_ping_bad_user_auth_denied(webapp):
     resp = webapp.get('/api/publish-ping', expect_errors=True)
     webapp.authorization = ('Basic', ('notauser', 'foobar'))
     assert resp.status_code == 401
+    assert resp.content_type == 'application/json'
